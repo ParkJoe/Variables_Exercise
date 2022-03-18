b82a2ca2afa641b5f3b9fffe70a46e9307c9ad71
Joe Park <joe_park@live.com>
2022-03-18 13:44:08 -0400

    first try 3/18/22

---
+++ Variables Exercise/Program.cs
@@ -0,0 +1,51 @@
+﻿// Variable Exercise Joe Park
+
+// Variable Declarations
+string car;
+
+int price;
+
+char levelOfLuxury;
+
+bool weOfferFinanceOptions;
+
+double aprPercentage;
+
+decimal myWallet;
+
+// Variable Initializations
+
+car = "Toyota Land Cruiser" + "Jeep Wrangler Rubicon" + "Hyundai Palisade";
+
+price = 90000 + 60000 + 55000;
+
+levelOfLuxury = '3';
+
+weOfferFinanceOptions = true;
+
+aprPercentage = 0.9;
+
+myWallet = 1.25m;
+
+Console.WriteLine($"Someday I hope to buy one of these cars {car}. They are pricey though {price}. In honesty, I like all {levelOfLuxury}. I hear they offer good financing deals {weOfferFinanceOptions}. They usually offer low interest rates for financing {aprPercentage}. Someday I will get one these cars because right now all I have in my wallet is {myWallet}.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
